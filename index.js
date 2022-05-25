8d6a4ec8f4b2e10a149dfda32918ace4bb9cfe2c
Petr Češka <ceskape@seznam.cz>
2022-05-25 16:13:37 +0200

    Changes from main copied to new_structure

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
-
-require('./server.js')
+var io = require('socket.io')(http);
+require('./server.js')(io);
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
+});
+
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
 });
 
 
--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
 const Game = require("./game.js");
 let users = {};
 
@@ -8,61 +5,63 @@ function getKeyByValue(object, value) {
     return Object.keys(object).find(key => object[key] === value);
 }
 
-io.on('connection', function (socket) {
-    console.log('a user connected');
+function server(io) {
+    io.on('connection', function (socket) {
+        console.log('a user connected');
 
-    socket.on("it's me", (name) => {
-        if (users[name] != null) {
-            console.log('returning user: ' + name + ' (after a client refresh). Welcome back!');
-        } else {
-            console.log('new user by the name of: ' + name);
-        }
-        users[name] = socket;
-    });
-
-    socket.on('chat message', function (msg) {
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
-    });
-
-    socket.on('shoot', function (msg) {
-        let [row, col] = msg.split(",");
-        console.log(row, ".", col)
-    });
-
-    socket.on('play', function (msg) {
-        let game = Game.createSingleplayer(getKeyByValue(users,socket), socket);
-
-        console.log('new game!: ' + msg);
-        let argArray = msg.split(" ");
-
-        for (let i in argArray) {
-            let arg = argArray[i].split("=");
-            console.log(arg[0]);
-            console.log(arg[1]);
-            switch (arg[0]) {
-                case "field":
-                    console.log(arg[1]);
-                    game.fieldSize = parseInt(arg[1]);
-                    break;
-                case "ships":
-                    console.log(arg[1]);
-                    game.shipsNum = parseInt(arg[1]);
-                    break;
-                // TODO add case: opponent for multi
-                default:
-                    console.log("emit");
-                    socket.emit('wrong parameters', `There is something wrong with argument "${argArray[i]}"`);
-                    return;
+        socket.on("it's me", (name) => {
+            if (users[name] != null) {
+                console.log('returning user: ' + name + ' (after a client refresh). Welcome back!');
+            } else {
+                console.log('new user by the name of: ' + name);
             }
-        }
-    });
+            users[name] = socket;
+        });
 
-    socket.on('disconnect', function () {
-        console.log('user disconnected');
-    });
-});
+        socket.on('chat message', function (msg) {
+            console.log('message: ' + msg);
+            io.emit('chat message', msg);
+        });
 
-http.listen(3000, function () {
-    console.log('listening on *:3000');
-});
+        socket.on('shoot', function (msg) {
+            let [row, col] = msg.split(",");
+            console.log(row, ".", col)
+        });
+
+        socket.on('play', function (msg) {
+            let game = Game.createSingleplayer(getKeyByValue(users,socket), socket);
+
+            console.log('new game!: ' + msg);
+            let argArray = msg.split(" ");
+            console.log(argArray)
+
+            for (let i in argArray) {
+                let arg = argArray[i].split("=");
+                if(arg[0] !== ''){
+                    switch (arg[0]) {
+                        case "field":
+                            console.log(arg[1]);
+                            game.fieldSize = parseInt(arg[1]);
+                            break;
+                        case "ships":
+                            console.log(arg[1]);
+                            game.shipsNum = parseInt(arg[1]);
+                            break;
+                        // TODO add case: opponent for multi
+                        default:
+                            console.log("emit");
+                            socket.emit('wrong parameters', `There is something wrong with argument "${argArray[i]}"`);
+                            return;
+                    }
+                }
+            }
+
+            socket.emit('construct game', `field=${game.fieldSize} ships=123456`);
+        });
+
+        socket.on('disconnect', function () {
+            console.log('user disconnected');
+        });
+    });
+}
+module.exports = server
