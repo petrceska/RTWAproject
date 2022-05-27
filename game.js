ea2e4d3d8f4c3688f1928f069b12e35c48152a76
David Valecký <xvalec01@vutbr.cz>
2022-05-27 03:50:49 +0200

    field a ship changed

--- game.js
+++ game.js
@@ -20,10 +20,9 @@ class Game {
 }
 
 class Player {
-    constructor(name, socket, ships, field) {
+    constructor(name, socket, field) {
         this.name = name;
         this.socket = socket;
-        this.ships = ships;
         this.field = field;
     }
 }
@@ -31,11 +30,13 @@ class Player {
 class Field {
     constructor(fieldSize) {
         this.fieldSize = fieldSize;
+        this.field = Array.from(Array(fieldSize), () => Array(fieldSize).fill(0));
     }
 
     set setSize(fieldSize) {
         if (fieldSize !== null) {
-            this.fieldSize = fieldSize
+            this.fieldSize = fieldSize;
+            this.field = Array.from(Array(fieldSize), () => Array(fieldSize).fill(0));
         }
     }
 
@@ -45,6 +46,15 @@ class Field {
         this.currentShipsNum = shipsNum;
     }
 
+
+    /*
+    shipUniquePositions() {
+        for (ship in this.ships) {
+
+        }
+    }
+    */
+    // check if the ship's position is on the position of another ship 
 }
 
 class Ship {
@@ -52,8 +62,55 @@ class Ship {
         this.type = type;
         this.pos_x = pos_x;
         this.pos_y = pos_y;
+        this.len
     }
-    // check if the ship's position is on the position of another ship 
+
+    set setType(type) {
+        this.type = type;
+        this.shipSize(type);
+    }
+
+    changeShipPosition(x = this.pos_x, y = this.pos_y) {
+        this.pos_x = x;
+        this.pos_y = y;
+    }
+
+    shipSize(type) {
+        switch (type) {
+            case "2x1":
+                this.len = 2;
+                this.width = 1;
+                break;
+            case "3x1":
+                this.len = 3;
+                this.width = 1;
+                break;
+            case "4x1":
+                this.len = 4;
+                this.width = 1;
+                break;
+            case "1x2":
+                this.len = 1;
+                this.width = 2;
+                break;
+            case "1x3":
+                this.len = 1;
+                this.width = 3;
+                break;
+            case "1x4":
+                this.len = 1;
+                this.width = 4;
+                break;
+            case "2x2":
+                this.len = 2;
+                this.width = 2;
+                break;
+            default:
+                this.len = 2;
+                this.width = 1;
+                return;
+        }
+    }
 
 }
 module.exports = Game
